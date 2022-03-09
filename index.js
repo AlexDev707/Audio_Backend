bb7baf2c67578d44faee151eb21650261177f114
AlexDev707 <saniachud007@gmail.com>
2022-03-09 19:57:52 +0200

    added branch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 const port = process.env.PORT;
 if (!port) {
   console.log('No port provided');
-  return;
+  process.exit(1);
 }
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ mongoose
   .then(() => console.log('Database connected successfully'))
   .catch((error) => console.log(error));
 
+  cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
+
 app.use(express.json());
 app.use(volleyball);
 app.use(helmet());
