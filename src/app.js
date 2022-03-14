f95390502c973d0556c2226dd635afc24448c479
MihailLadyk <lp346426201@gmail.com>
2022-03-14 19:34:15 +0200

    1

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const cloudinary = require("cloudinary");
 const app = express();
 
 mongoose
@@ -12,11 +12,11 @@ mongoose
   .then(() => console.log("Database connected successfully"))
   .catch((error) => console.log(error));
 
-  cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET 
-  });
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.use(express.json());
 app.use(volleyball);
